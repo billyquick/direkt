3d189188bf1bcb72caaaa3cbf3990ee0a1fcc1a5
billyquick <billyqquick@gmail.com>
2016-08-14 23:38:47 -0400

    QoL updates

--- direkt.js
+++ direkt.js
@@ -23,7 +23,45 @@ function hideGame(){
 
 function transition(thisBtn){
   $(".game").slideUp(function complete(){
-    $(".game").slideDown();
+    if(thisBtn != "how-to-play-btn"){
+      $(".tutorial").css("display", "none");
+    } else {
+      $(".tutorial").css("display", "block");
+    }
+    $(".game").slideDown(function complete(){
+      startGame(thisBtn);
+    });
   });
   prevSelectedBtn = thisBtn;
+}
+
+// var arrowValues = [];
+function startGame(difficulty){
+  switch(difficulty){
+    case "easy-btn":
+      //game
+      /* need to generate random arrow, set value of arrow created to a variable and put it
+       * into an array. From there, I need to generate another random arrow and put it's value
+       * into the same array at index [1]. Then compare the the value of the arrow at index [0]
+       * with the user's input. Probably will look something like this:
+       arrowValues[generateRandomArrow()];
+       */
+      break;
+    case "normal-btn":
+      //game
+      break;
+    case "hard-btn":
+      //game
+      break;
+    case "impossible-btn":
+      //game
+      break;
+    default:
+      break;
+  }
+}
+
+function generateRandomArrow(){
+  /* keycodes for arrows are 37-40 - want to generate a random number between those.
+   */
 }
